615b7828bd9515e371b65deaee5e5d139ca4cb88
Nicholas Blair <nblair@doit.wisc.edu>
2012-09-06 10:13:58 -0500

    update license

--- src/main/java/nblair/pipeline/PipelineThreadPoolExecutor.java
+++ src/main/java/nblair/pipeline/PipelineThreadPoolExecutor.java
@@ -1,6 +1,18 @@
-/*******************************************************************************
- *  Copyright 2012 The Board of Regents of the University of Wisconsin System.
- *******************************************************************************/
+/**
+ *    Copyright 2012 Nicholas Blair
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package nblair.pipeline;
 
 import java.util.concurrent.LinkedBlockingQueue;
@@ -15,7 +27,6 @@ import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
  * Sub class of {@link ThreadPoolExecutor} tailored for {@link AbstractMigrationStep}.
  *  
  * @author Nicholas Blair
- * @version $Id: PipelineThreadPoolExecutor.java $
  */
 public class PipelineThreadPoolExecutor extends ThreadPoolExecutor {
 
--- src/main/java/nblair/pipeline/WorkUnitCollection.java
+++ src/main/java/nblair/pipeline/WorkUnitCollection.java
@@ -1,5 +1,17 @@
 /**
- * 
+ *    Copyright 2012 Nicholas Blair
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package nblair.pipeline;
@@ -7,6 +19,12 @@ package nblair.pipeline;
 import java.util.Collection;
 
 /**
+ * Extension of {@link Collection} that adds method to designate that even
+ * if the {@link Collection#isEmpty()}, it may be "expecting more" to eventually show up.
+ * {@link #isComplete()} will return false until no more items are expected.
+ * 
+ * Callers that populate this {@link WorkUnitCollection} must invoke {@link #signalCompletion()}
+ * to indicate no more items are expected. 
  * @author Nicholas Blair
  */
 public interface WorkUnitCollection<E> extends Collection<E> {
