540bb0031f2c48742b0f1bd40e596c5a14c82035
Nicholas Blair <npblair@wisc.edu>
2013-01-23 13:42:32 -0600

    update license and add notice, switch to sonatype parent

--- src/main/java/nblair/pipeline/PipelineStep.java
+++ src/main/java/nblair/pipeline/PipelineStep.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2012 Nicholas Blair
+ *    Copyright 2012, Board of Regents of the University of Wisconsin System
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-
 package nblair.pipeline;
 
 /**
